86b82d5be31f6d942ff00303915e3000168543fe
Indalu <endalukalbesa511@gmail.com>
2025-05-22 01:18:52 +0400

    last commit

--- hms-frontend/src/context/userContext.js
+++ hms-frontend/src/context/userContext.js
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 const UserContext = createContext();
 export const useUserContext = () => useContext(UserContext);
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState("doctor"); // default user is admin
+  const [user, setUser] = useState(""); // default user is admin
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [userRole,setUserRole] = useState("");
   useEffect(() => {
