06b43ff3ee37b1006869037f16d505d96c1fe312
Indalu <endalukalbesa511@gmail.com>
2025-05-22 01:13:00 +0400

    change

--- hms-frontend/src/context/userContext.js
+++ hms-frontend/src/context/userContext.js
@@ -4,14 +4,9 @@ import { useEffect } from "react";
 const UserContext = createContext();
 export const useUserContext = () => useContext(UserContext);
 export const UserProvider = ({ children }) => {
-<<<<<<< HEAD
-  const [user, setUser] = useState(""); // default user is admin
-=======
-
   const [user, setUser] = useState("doctor"); // default user is admin
->>>>>>> 091cbd5e14f920cdfe561941b4743d77b5e3c058
   const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userRole,setUserRole] = useState("patient");
+  const [userRole,setUserRole] = useState("");
   useEffect(() => {
     if (user) {
       setIsLoggedIn(true);
